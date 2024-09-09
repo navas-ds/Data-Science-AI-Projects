d6183b42b1bf4fed0349f5b169de4614095d6fdd
navas-ds <navas.ktdi@gmail.com>
2024-09-09 20:32:07 +0530

    Update app.py

--- Machine_Learning/Loan_eligibility_prediction/app.py
+++ Machine_Learning/Loan_eligibility_prediction/app.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 
 # Load the trained model
-with open('loan_model.pkl', 'rb') as file:
+with open('model.pkl','rb') as file:
     model = pickle.load(file)
 
 # Title of the app
